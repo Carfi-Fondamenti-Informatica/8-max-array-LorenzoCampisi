7186d3894d6a44160b20bdb58b2373400d4aa60c
LorenzoCampisi <91668709+LorenzoCampisi@users.noreply.github.com>
2021-12-07 15:49:26 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main() {
 
  int dimensione;
- cout<<"inserisci dimensione"<<endl;
+
  cin>>dimensione;
  float array[dimensione];
 
   for(int i=0; i<dimensione; i++){
-      cout<<"inserisci valore "<<i<<" array"<<endl;
+      
       cin>>array[i];
   }
 
   float ris=max(dimensione,array);
-  cout<<"il valore maggiore inserito e': "<<ris<<endl;
+  cout<<ris<<endl;
 
     return 0;
 }
